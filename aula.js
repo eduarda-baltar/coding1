645cf06e0c1ad91cb12cd9b0d4ce5f6d06d55593
eduarda-baltar <maria.esnaty@edu.pe.senac.br>
2024-09-18 13:54:02 +0000

    criação de prompt

---
+++ aula.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')();
+
+let nome = prompt('Olá, qual o seu nome?')
+console.log(`Olá, ${nome}!`)
+ 
+let a = prompt('Tudo bem?')
+console.log ('Legal!')
+
+let b =prompt('Como posso lhe ajudar?')
